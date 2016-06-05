b46c8e6554e5bba6d017c590ec8315eaea0656cb
scott89 <scott89@github.com>
2016-06-05 09:23:11 +0800

    train lenet

--- examples/mnist/train_lenet.sh
+++ examples/mnist/train_lenet.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-./build/tools/caffe train --solver=examples/mnist/lenet_solver.prototxt
+./build/tools/caffe train --solver=examples/mnist/lenet_solver.prototxt 2>&1 | tee logi1.txt
+
