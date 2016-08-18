f42e4b0ae0ddbfa628ec49bb4c2d0e75c6fd0aef
scott89 <scott89@github.com>
2016-08-18 09:47:04 +0800

    finetuning without mask

--- examples/cvpr17/train.sh
+++ examples/cvpr17/train.sh
@@ -12,7 +12,14 @@
 ##./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17/ILT_iter_126000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log4.txt 
 ## Resume from iteration: 
 
-./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17/ILT_iter_284000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log5.txt 
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17/ILT_iter_284000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log5.txt 
+
+# finetuning vgg16
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=/home/lijun/Research/Code/CVPR17/model/vgg16CAM_train_iter_90000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log2-1.txt 
+
+# decrease learning rate at iteration 188000
+./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-finetune/ILT_iter_188000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log2-2.txt 
+
 #./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17/ILT_iter_2000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log2.txt 
 
 #./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17/ILT_iter_76000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log3.txt 
