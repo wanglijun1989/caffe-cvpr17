6f16f034f96e13629cfb1b69bc85cca0a8e4808d
scott89 <scott89@github.com>
2016-08-23 19:14:32 +0800

    Training with upper bounded ReLU

--- examples/cvpr17/train.sh
+++ examples/cvpr17/train.sh
@@ -1,4 +1,7 @@
-#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=/home/lijun/Research/Code/FCT_scale_base/model/VGG_ILSVRC_16_layers.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-6.txt 
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=/home/lijun/Research/Code/FCT_scale_base/model/VGG_ILSVRC_16_layers.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-7-1.txt 
+
+./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-smooth/ILT-from-188000-5_iter_58000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-7-1.txt 
+
 # no major changes, continue training
 #./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-smooth/ILT-from-188000-6_iter_1740.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-6-2.txt 
 
@@ -9,4 +12,4 @@
 #./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-smooth/ILT-from-188000-6_iter_18000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-6-4.txt 
 
 # change conv6 and genmap learning rate to 100000
-./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-smooth/ILT-from-188000-6_iter_18000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-6-4.txt 
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-smooth/ILT-from-188000-6_iter_18000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-6-4.txt 
