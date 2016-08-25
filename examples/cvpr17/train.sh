d33429aebf80783684fa1e6f532faa2cc83148e2
scott89 <scott89@github.com>
2016-08-25 21:40:10 +0800

    Training ILT with BN

--- examples/cvpr17/train.sh
+++ examples/cvpr17/train.sh
@@ -1,6 +1,5 @@
 
-#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-smooth/ILT-from-188000-5_iter_58000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-7-1.txt 
-
-#change max_value in smooth_pooling from 32 to 64
-./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-smooth/ILT-from-188000-7_iter_6000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log-smooth-7-2.txt 
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=/home/lijun/Research/Code/CVPR17/model/vgg16CAM_train_iter_90000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-2.txt 
+#resotre from iter 115000
+./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-ILT/ILT_iter_115000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-2.txt 
 
