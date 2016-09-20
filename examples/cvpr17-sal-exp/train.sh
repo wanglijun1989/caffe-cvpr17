601c2f978cd4491582c0b646749a8d9bace37b9f
scott89 <scott89@github.com>
2016-09-20 14:55:50 +0800

    training both sal-1 with softmaxhardstraploss and sal1-exp with softmaxloss

--- examples/cvpr17-sal-exp/train.sh
+++ examples/cvpr17-sal-exp/train.sh
@@ -1,9 +1,9 @@
 
-#./build/tools/caffe train -solver=examples/cvpr17-sal/solver.prototxt -weights=models/cvpr17-ILT/ILT-3_iter_6000.caffemodel   2>&1 | tee examples/cvpr17-sal/log-sal-1.txt 
+./build/tools/caffe train -solver=examples/cvpr17-sal-exp/solver.prototxt -weights=models/cvpr17-ILT/ILT-3_iter_6000.caffemodel -gpu=1  2>&1 | tee examples/cvpr17-sal-exp/log-sal-1.txt 
 #resotre from iter , snapshot interval change to 1000
 #./build/tools/caffe train -solver=examples/cvpr17-sal/solver.prototxt -snapshot=models/cvpr17-sal/sal-1_iter_4000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17-sal/log-sal-2.txt 
 # restore from iter 57000
-./build/tools/caffe train -solver=examples/cvpr17-sal/solver.prototxt -snapshot=models/cvpr17-sal/sal-1_iter_57000.solverstate -gpu=0 2>&1 | tee examples/cvpr17-sal/log-sal-3.txt 
+#./build/tools/caffe train -solver=examples/cvpr17-sal/solver.prototxt -snapshot=models/cvpr17-sal/sal-1_iter_57000.solverstate -gpu=0 2>&1 | tee examples/cvpr17-sal/log-sal-3.txt 
 
 #./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-ILT/ILT-_iter_44000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-3.txt 
 
