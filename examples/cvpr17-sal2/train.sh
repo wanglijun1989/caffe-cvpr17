5156a43b1314b577c9490f33f5e267b98abfe72c
scott89 <scott89@github.com>
2016-09-21 16:55:09 +0800

    training sal2 with bn

---
+++ examples/cvpr17-sal2/train.sh
@@ -0,0 +1,10 @@
+
+./build/tools/caffe train -solver=examples/cvpr17-sal2/solver.prototxt -weights=models/cvpr17-ILT/ILT-3_iter_6000.caffemodel   2>&1 | tee examples/cvpr17-sal2/log-sal2-1.txt 
+#resotre from iter , snapshot interval change to 1000
+#./build/tools/caffe train -solver=examples/cvpr17-sal/solver.prototxt -snapshot=models/cvpr17-sal/sal-1_iter_4000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17-sal/log-sal-2.txt 
+# restore from iter 57000
+#./build/tools/caffe train -solver=examples/cvpr17-sal/solver.prototxt -snapshot=models/cvpr17-sal/sal-1_iter_57000.solverstate -gpu=0 2>&1 | tee examples/cvpr17-sal/log-sal-3.txt 
+
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-ILT/ILT-_iter_44000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-3.txt 
+
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-ILT/ILT-3_iter_74000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-4.txt 
