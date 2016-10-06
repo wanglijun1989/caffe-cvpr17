aba03caa2dae19ce8d5408d6f4dce43c7fbe1015
scott89 <scott89@github.com>
2016-10-06 09:42:11 +0800

    ILT-pretraion-exp1-1 and ILT-pretraion-exp3

---
+++ examples/cvpr17-ILT-pretrain-exp1/train.sh
@@ -0,0 +1,15 @@
+
+#./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain-exp1/solver.prototxt -weights=/home/lijun/Research/Code/CVPR17/model/vgg16CAM_train_iter_90000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain-exp1/log-ip-1.txt
+#resotre from iter 4000
+./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain-exp1/solver.prototxt -snapshot=models/cvpr17-ILT-pretrain-exp1/ip-1_iter_4000.solverstate -gpu=0 2>&1 | tee examples/cvpr17-ILT-pretrain-exp1/log-ip-1-1.txt 
+
+#resotre from iter 102500
+#./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain-exp2/solver.prototxt -snapshot=models/cvpr17-ILT-pretrain-exp2/ip-1_iter_102500.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain-exp2/log-ip-1-3.txt 
+
+#restore from iter 80000, decrease lr by 0.1
+#./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain-exp2/solver.prototxt -snapshot=models/cvpr17-ILT-pretrain-exp2/ip-1_iter_14000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain-exp2/log-ip-2.txt 
+
+
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-ILT/ILT-_iter_44000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-3.txt 
+
+#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-ILT/ILT-3_iter_74000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-4.txt 
---
+++ examples/cvpr17-ILT-pretrain-exp3/cvpr17-ILT-pretrain-exp/test.sh
@@ -0,0 +1,7 @@
+#./build/tools/caffe test -gpu=0,1 -model=examples/cvpr17/train_val.prototxt -weights=models/cvpr17-ILT/ILT-_iter_109000.caffemodel -iterations=577 
+
+#./build/tools/caffe test -gpu=0,1 -model=examples/cvpr17/train_val.prototxt -weights=models/cvpr17-ILT/ILT-_iter_107000.caffemodel -iterations=577 
+
+#./build/tools/caffe test -gpu=0,1 -model=examples/cvpr17/train_val.prototxt -weights=models/cvpr17-ILT/ILT-_iter_121000.caffemodel -iterations=577 
+
+./build/tools/caffe test -gpu=0,1 -model=examples/cvpr17/train_val.prototxt -weights=models/cvpr17-ILT/ILT-_iter_123000.caffemodel -iterations=577 
