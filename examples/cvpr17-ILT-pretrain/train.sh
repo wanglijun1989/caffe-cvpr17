1673d54aea7de214b230c2d4dc62ab2c2ec853c3
scott89 <scott89@github.com>
2016-10-07 10:28:33 +0800

    ILT-pretrain-1

--- examples/cvpr17-ILT-pretrain/train.sh
+++ examples/cvpr17-ILT-pretrain/train.sh
@@ -1,7 +1,14 @@
 
-./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain/solver.prototxt -weights=/home/lijun/Research/Code/CVPR17/model/vgg16CAM_train_iter_90000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain/log-ip-1-exp.txt 
-#resotre from iter 115000
-#./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -snapshot=models/cvpr17-ILT/ILT_iter_115000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-2.txt 
+./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain/solver.prototxt -weights=/home/lijun/Research/Code/CVPR17/model/vgg16CAM_train_iter_90000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain/log-ip-1.txt
+#resotre from iter 49000
+#./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain-exp2/solver.prototxt -snapshot=models/cvpr17-ILT-pretrain-exp2/ip-1_iter_49000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain-exp2/log-ip-2.txt 
+
+#resotre from iter 102500
+#./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain-exp2/solver.prototxt -snapshot=models/cvpr17-ILT-pretrain-exp2/ip-1_iter_102500.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain-exp2/log-ip-1-3.txt 
+
+#restore from iter 80000, decrease lr by 0.1
+#./build/tools/caffe train -solver=examples/cvpr17-ILT-pretrain-exp2/solver.prototxt -snapshot=models/cvpr17-ILT-pretrain-exp2/ip-1_iter_14000.solverstate -gpu=0,1 2>&1 | tee examples/cvpr17-ILT-pretrain-exp2/log-ip-2.txt 
+
 
 #./build/tools/caffe train -solver=examples/cvpr17/solver.prototxt -weights=models/cvpr17-ILT/ILT-_iter_44000.caffemodel -gpu=0,1 2>&1 | tee examples/cvpr17/log-ILT-3.txt 
 
